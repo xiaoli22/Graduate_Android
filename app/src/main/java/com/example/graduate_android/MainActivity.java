b5f55363204674f93a70356c5205ccc8bcd6c5ea
“xiaoli22” <1879847326@qq.com>
2023-03-03 20:33:29 +0800

    底部导航栏 ButtonAppBar

--- app/src/main/java/com/example/graduate_android/MainActivity.java
+++ app/src/main/java/com/example/graduate_android/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.graduate_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigation = findViewById(R.id.bottomNavigation);
+        bottomNavigation.setBackground(null);
+
+
     }
 }
